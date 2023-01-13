2a9e09ceea41abf3ddc6f012e9508d99581652c7
hmdmostafa <100584690+hmdmostafa@users.noreply.github.com>
2023-01-13 20:28:59 +0300

    Add files via upload

---
+++ pyUltroid/dB/__init__.py
@@ -0,0 +1,56 @@
+from .. import run_as_module
+
+if not run_as_module:
+    from ..exceptions import RunningAsFunctionLibError
+
+    raise RunningAsFunctionLibError(
+        "You are running 'pyUltroid' as a functions lib, not as run module. You can't access this folder.."
+    )
+
+from .. import *
+
+DEVLIST = [
+    5502537272,  # @ZQ_LO
+]
+
+ULTROID_IMAGES = [
+    f"https://graph.org/file/{_}.jpg"
+    for _ in [
+        "b3e0cb631d36672092852",
+        "56b2ec14ede1b877cbdf7",
+        "a6fb8a39f0aebe1d98570",
+    ]
+]
+
+stickers = [
+    "CAADAQADeAIAAm_BZBQh8owdViocCAI",
+    "CAADAQADegIAAm_BZBQ6j8GpKtnrSgI",
+    "CAADAQADfAIAAm_BZBQpqC84n9JNXgI",
+    "CAADAQADfgIAAm_BZBSxLmTyuHvlzgI",
+    "CAADAQADgAIAAm_BZBQ3TZaueMkS-gI",
+    "CAADAQADggIAAm_BZBTPcbJMorVVsQI",
+    "CAADAQADhAIAAm_BZBR3lnMZRdsYxAI",
+    "CAADAQADhgIAAm_BZBQGQRx4iaM4pQI",
+    "CAADAQADiAIAAm_BZBRRF-cjJi_QywI",
+    "CAADAQADigIAAm_BZBQQJwfzkqLM0wI",
+    "CAADAQADjAIAAm_BZBQSl5GSAT0viwI",
+    "CAADAQADjgIAAm_BZBQ2xU688gfHhQI",
+    "CAADAQADkAIAAm_BZBRGuPNgVvkoHQI",
+    "CAADAQADpgIAAm_BZBQAAZr0SJ5EKtQC",
+    "CAADAQADkgIAAm_BZBTvuxuayqvjhgI",
+    "CAADAQADlAIAAm_BZBSMZdWN2Yew1AI",
+    "CAADAQADlQIAAm_BZBRXyadiwWGNkwI",
+    "CAADAQADmAIAAm_BZBQDoB15A1jS1AI",
+    "CAADAQADmgIAAm_BZBTnOLQ8_d72vgI",
+    "CAADAQADmwIAAm_BZBTve1kgdG0Y5gI",
+    "CAADAQADnAIAAm_BZBQUMyFiylJSqQI",
+    "CAADAQADnQIAAm_BZBSMAe2V4pwhNgI",
+    "CAADAQADngIAAm_BZBQ06D92QL_vywI",
+    "CAADAQADnwIAAm_BZBRw7UAbr6vtEgI",
+    "CAADAQADoAIAAm_BZBRkv9DnGPXh_wI",
+    "CAADAQADoQIAAm_BZBQwI2NgQdyKlwI",
+    "CAADAQADogIAAm_BZBRPHJF3XChVLgI",
+    "CAADAQADowIAAm_BZBThpas7rZD6DAI",
+    "CAADAQADpAIAAm_BZBQcC2DpZcCw1wI",
+    "CAADAQADpQIAAm_BZBQKruTcEU4ntwI",
+]
